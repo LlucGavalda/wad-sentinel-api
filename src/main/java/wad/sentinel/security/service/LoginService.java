e37ff7a8388bd9944c81465f28f1f82d6127f776
LlucGavalda <lluc@webadvanced.cat>
2025-03-27 17:13:32 +0100

    deleted security

--- src/main/java/wad/sentinel/security/service/LoginService.java
+++
@@ -1,9 +0,0 @@
-package wad.sentinel.security.service;
-
-import wad.sentinel.security.dto.LoginInDto;
-import wad.sentinel.security.dto.LoginOutDto;
-
-public interface LoginService {
-
-    public LoginOutDto login(LoginInDto loginRequest);
-}
